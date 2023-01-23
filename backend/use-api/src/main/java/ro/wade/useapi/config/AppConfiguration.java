a25a01c3ac372673a3a31413250010e3e03589b2
Silviu Ilas <silviuilas@gmail.com>
2023-01-23 20:19:29 +0200

    Add hotfix

--- backend/use-api/src/main/java/ro/wade/useapi/config/AppConfiguration.java
+++ backend/use-api/src/main/java/ro/wade/useapi/config/AppConfiguration.java
@@ -1,17 +1,20 @@
 package ro.wade.useapi.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+
+@Configuration
 public class AppConfiguration {
     @Bean
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
+        config.addAllowedOrigin("http://localhost:4200");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         source.registerCorsConfiguration("/**", config);
