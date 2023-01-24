0868424724c1c7927790b1a833891900851de32b
GeorgicaMarius <61226962+GeorgicaMarius@users.noreply.github.com>
2023-01-24 12:28:49 +0200

    improved collection query helper

--- backend/use-api/src/main/java/ro/wade/useapi/helpers/UseDsCollectionQueryHelper.java
+++ backend/use-api/src/main/java/ro/wade/useapi/helpers/UseDsCollectionQueryHelper.java
@@ -8,11 +8,11 @@ import org.springframework.stereotype.Service;
 import ro.wade.useapi.models.UseCollectionQueryDto;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 @Service
 public class UseDsCollectionQueryHelper {
+
     private final String useDatasetEndpointUrl;
     private final String prefixesBlock = "" +
             "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
@@ -25,47 +25,7 @@ public class UseDsCollectionQueryHelper {
         this.useDatasetEndpointUrl = useDatasetEndpointUrl;
     }
 
-    private UseCollectionQueryDto resultSetToSingleCollectionDto(ResultSet results) {
-        UseCollectionQueryDto collection = new UseCollectionQueryDto();
-        if (results.hasNext()) {
-            QuerySolution sol = results.nextSolution();
-            collection.collectionId = sol.getLiteral("collectionId").getString();
-            collection.collectionTitle = sol.getLiteral("collectionTitle").getString();
-            (collection.photoIds = new ArrayList<>()).add(sol.getLiteral("photoId").getString());
-        }
-        while (results.hasNext()) {
-            QuerySolution sol = results.nextSolution();
-            collection.photoIds.add(sol.getLiteral("photoId").getString());
-        }
-        return collection;
-    }
-
-    private List<UseCollectionQueryDto> resultSetToMultipleCollectionDto(ResultSet results) {
-        HashMap<String, UseCollectionQueryDto> map = new HashMap<>();
-
-        while (results.hasNext()) {
-            QuerySolution sol = results.nextSolution();
-            String collectionId = sol.contains("collectionId") ? sol.getLiteral("collectionId").getString() : null;
-            String collectionTitle = sol.contains("collectionTitle") ? sol.getLiteral("collectionTitle").getString() : null;
-            String photoId = sol.contains("photoId") ? sol.getLiteral("photoId").getString() : null;
-
-            if (map.containsKey(collectionId)) {
-                map.get(collectionId).photoIds.add(photoId);
-            } else {
-                UseCollectionQueryDto collection = new UseCollectionQueryDto();
-                collection.collectionId = collectionId;
-                collection.collectionTitle = collectionTitle;
-                (collection.photoIds = new ArrayList<>()).add(photoId);
-                map.put(collectionId, collection);
-            }
-        }
-        return new ArrayList<>(map.values());
-    }
-
-    private ResultSet executeBasicQuery(String filterBlock, String sortBlock, Integer offset, Integer limit) {
-        if (filterBlock == null) filterBlock = "";
-        if (sortBlock == null) sortBlock = "";
-
+    private UseCollectionQueryDto executeGetCollectionByIdQuery(String collectionId, boolean verbose) {
         ParameterizedSparqlString pss = new ParameterizedSparqlString();
         pss.setCommandText("" +
                 prefixesBlock +
@@ -76,35 +36,43 @@ public class UseDsCollectionQueryHelper {
                 "                 rdfs:label ?collectionTitle ;\n" +
                 "                 rdfs:member ?entryItem .\n" +
                 "  ?entryItem schema:identifier ?photoId .\n" +
-                filterBlock +
-                "}\n" +
-                sortBlock + "\n");
-        if (offset != null)
-            pss.append("OFFSET " + offset + "\n");
-        if (limit != null)
-            pss.append("LIMIT " + limit + "\n");
-
+                "  FILTER (?collectionId = ?injectedCollectionId)\n" +
+                "}\n");
+        pss.setLiteral("injectedCollectionId", collectionId);
         // see the query string with injected values
-        // System.out.println(pss);
+        if (verbose)
+            System.out.println(pss);
         Query query = pss.asQuery();
 
-        ResultSet resultSetCopy;
         try (QueryExecution queryExec = QueryExecutionHTTP.service(useDatasetEndpointUrl).query(query).build()) {
             ResultSet results = queryExec.execSelect();
-            resultSetCopy = ResultSetFactory.copyResults(results);
+            if (results.hasNext()) {
+                QuerySolution sol = results.nextSolution();
+                UseCollectionQueryDto collection = new UseCollectionQueryDto();
+                collection.collectionId = sol.getLiteral("collectionId").getString();
+                collection.collectionTitle = sol.getLiteral("collectionTitle").getString();
+                (collection.photoIds = new ArrayList<>()).add(sol.getLiteral("photoId").getString());
+                return collection;
+            }
         }
-        return resultSetCopy;
+        return null;
     }
 
-    private List<String> executeGetCollectionIdsQuery(Integer offset, Integer limit) {
+    private List<String> executeGetCollectionIdsQuery(String filterBlock, String sortBlock, Integer offset, Integer limit) {
+        if (filterBlock == null) filterBlock = "";
+        if (sortBlock == null) sortBlock = "";
+
         ParameterizedSparqlString pss = new ParameterizedSparqlString();
         pss.setCommandText("" +
                 prefixesBlock +
                 "\n" +
                 "SELECT * WHERE {\n" +
                 "  ?collectionRes rdf:type rdf:Bag ;\n" +
-                "                 schema:identifier ?collectionId .\n" +
-                "}\n");
+                "                 schema:identifier ?collectionId ;\n" +
+                "                 rdfs:label ?collectionTitle .\n" +
+                filterBlock +
+                "}\n" +
+                sortBlock + "\n");
         if (offset != null)
             pss.append("OFFSET " + offset + "\n");
         if (limit != null)
@@ -125,16 +93,8 @@ public class UseDsCollectionQueryHelper {
         return collectionIds;
     }
 
-    private <T> List<T> clampedSublist(List<T> list, int offset, int limit) {
-        return list.subList(
-                Math.min(list.size(), offset),
-                Math.min(list.size(), offset + limit));
-    }
-
-    public UseCollectionQueryDto getCollectionById(String collectionId) {
-        String filterBlock = "  FILTER (?collectionId = \"" + collectionId + "\")\n";
-        ResultSet resultSet = executeBasicQuery(filterBlock, "", 0, null);
-        return resultSetToSingleCollectionDto(resultSet);
+    public UseCollectionQueryDto getCollectionById(String collectionId, boolean verbose) {
+        return executeGetCollectionByIdQuery(collectionId, verbose);
     }
 
     public List<UseCollectionQueryDto> getCollectionsFilter(Integer offset, Integer limit, String collectionTitle) {
@@ -142,24 +102,24 @@ public class UseDsCollectionQueryHelper {
         if (collectionTitle != null)
             filterBlock = String.format("  FILTER (regex (?collectionTitle, \"^%s$\", \"i\"))\n", collectionTitle);
 
-        if (filterBlock != null) {
-            ResultSet resultSet = executeBasicQuery(filterBlock, null, 0, null);
-            return clampedSublist(resultSetToMultipleCollectionDto(resultSet), offset, limit);
-        } else {
-            List<String> collectionIds = executeGetCollectionIdsQuery(offset, limit);
-            List<UseCollectionQueryDto> collections = new ArrayList<>();
-            for (String collectionId : collectionIds) {
-                collections.add(getCollectionById(collectionId));
-            }
-            return collections;
+        List<String> collectionIds = executeGetCollectionIdsQuery(filterBlock, null, offset, limit);
+        List<UseCollectionQueryDto> collections = new ArrayList<>();
+        for (String collectionId : collectionIds) {
+            collections.add(getCollectionById(collectionId, false));
         }
+        return collections;
     }
 
     public List<UseCollectionQueryDto> getCollectionsSearch(Integer offset, Integer limit, String collectionTitle) {
         String filterBlock = null;
         if (collectionTitle != null)
             filterBlock = String.format("  FILTER (regex (?collectionTitle, \"%s\", \"i\"))\n", collectionTitle);
-        ResultSet resultSet = executeBasicQuery(filterBlock, null, 0, null);
-        return clampedSublist(resultSetToMultipleCollectionDto(resultSet), offset, limit);
+
+        List<String> collectionIds = executeGetCollectionIdsQuery(filterBlock, null, offset, limit);
+        List<UseCollectionQueryDto> collections = new ArrayList<>();
+        for (String collectionId : collectionIds) {
+            collections.add(getCollectionById(collectionId, false));
+        }
+        return collections;
     }
 }
