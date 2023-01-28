1ceecb7197158c105d1627ee72864416ef81331b
Silviu Ilas <silviuilas@gmail.com>
2023-01-28 22:47:36 +0200

    Fix scroll + height

--- frontend/src/app/shared/directives/scroll-watch.directive.ts
+++ frontend/src/app/shared/directives/scroll-watch.directive.ts
@@ -1,16 +1,17 @@
-import { Directive, ElementRef, EventEmitter, HostListener, Output } from "@angular/core";
+import {Directive, ElementRef, EventEmitter, HostListener, Output} from "@angular/core";
 
 @Directive({
   selector: '[scrollWatch]',
 })
-export class ScrollWatchDirective {
+export class ScrollWatchDirective  {
   @Output() scrollReachedLowerBound: EventEmitter<void> = new EventEmitter();
 
-  constructor(private el: ElementRef) {}
+  constructor(private el: ElementRef) {
+  }
 
   @HostListener('scroll', ['$event'])
   scrollIt(scroll: any) {
-    if(scroll.target.scrollTop + 600 >= scroll.target.scrollTopMax){
+    if (scroll.target.scrollTop + scroll.target.clientHeight  + 600 >= scroll.target.scrollHeight) {
       this.scrollReachedLowerBound.emit();
     }
   }
