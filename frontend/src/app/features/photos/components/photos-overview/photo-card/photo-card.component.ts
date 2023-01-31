6623d6e106d0d77d7d6e1e6b536422e51aac96bb
RobertFrentz <30467166+RobertFrentz@users.noreply.github.com>
2023-01-31 21:28:25 +0200

    Middle Click compatibility with collection routing

--- frontend/src/app/features/photos/components/photos-overview/photo-card/photo-card.component.ts
+++ frontend/src/app/features/photos/components/photos-overview/photo-card/photo-card.component.ts
@@ -1,4 +1,6 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { first } from 'rxjs';
 import { Photo } from '../../../types/photo.type';
 
 @Component({
@@ -6,10 +8,28 @@ import { Photo } from '../../../types/photo.type';
   templateUrl: './photo-card.component.html',
   styleUrls: ['./photo-card.component.scss'],
 })
-export class PhotoCardComponent {
+export class PhotoCardComponent implements OnInit {
   @Input() photo!: Photo;
 
+  url: string = '';
+
   errorOnImageLoading = false;
+
+  constructor(private activatedRoute: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.url = `/photos/${this.photo.photoId}`;
+    this.getRouteParams();
+  }
+
+  getRouteParams(): void {
+    this.activatedRoute.paramMap.pipe(first()).subscribe((map) => {
+      const routeCollectionId = map.get('id');
+      if (routeCollectionId) {
+        this.url = `/collections/${routeCollectionId}/${this.photo.photoId}`;
+      }
+    });
+  }
 
   onErrorLoadImage(): void {
     this.errorOnImageLoading = true;
